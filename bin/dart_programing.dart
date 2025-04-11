ff810504b6c64f7b1bb51e941afb29a5e801e848
prudhviraj <sprudhviraj17@gmail.com>
2025-04-11 11:56:13 +0530

    dart prog

---
+++ bin/dart_programing.dart
@@ -0,0 +1,5 @@
+import 'package:dart_programing/dart_programing.dart' as dart_programing;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_programing.calculate()}!');
+}
---
+++ test/dart_programing_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_programing/dart_programing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
